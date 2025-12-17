7aabb0aeefad5c029147f6992cc6379aa0d2b962
LuciaraPereira <luciarapereir4@gmail.com>
2025-12-17 11:19:31 -0300

    responsividade

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,8 @@
-fetch('particles-config.json')  
-  .then(response => response.json())  
-  .then(config => {
-    // Efeito de partículas feito com a biblioteca particles.js de Vincent Garreau
-    particlesJS("particles-js", config);
-  })
-  .catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
+const navegacao = document.getElementById('links-nav');
+const menu = document.getElementById('icon-menu');
 
-
-  const navegacao = document.getElementById('links-nav');
-  const menu = document.getElementById('icon-menu');
-
-  menu.addEventListener('click', () => {
+menu.addEventListener('click', () => {
     navegacao.classList.toggle('ativo')
-  })
+})
 
 
